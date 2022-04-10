96ff8f34e55fbe17d9ea41abe9070185b2503d2b
cezarybak <cezary.bak0@gmail.com>
2022-04-10 19:14:30 +0200

    feat: accessibility change

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -17,23 +17,28 @@ export const Navbar = ({ pagesData }: Props) => {
       <div className="flex justify-between">
         <div className="flex  gap-x-1 items-center md:gap-x-[97px]">
           <Link href={"/"} passHref>
-            <a className="cursor-pointer ">
+            <a aria-label="home-navigation" className="cursor-pointer ">
               <Logo className="fill-BLACK  w-[89px] h-[32px]" />
             </a>
           </Link>
           <div className="flex gap-x-6 md:gap-x-12">
-            {pagesData.map(({ id, url }) => (
-              <Link key={id} href={url}>
-                <a
-                  className={`capitalize text-[12px] hover:font-medium ${
-                    asPath == url &&
-                    "underline underline-offset-8 decoration-2 decoration-DARK"
-                  }  md:text-[16px]`}
-                >
-                  {url.replace("/", "")}
-                </a>
-              </Link>
-            ))}
+            {pagesData.map(({ id, url }) => {
+              const trimURL = url.replace("/", "");
+
+              return (
+                <Link key={id} href={url}>
+                  <a
+                    aria-label={`${trimURL}-navigation`}
+                    className={`capitalize text-[12px] hover:font-medium ${
+                      asPath == url &&
+                      "underline underline-offset-8 decoration-2 decoration-DARK"
+                    }  md:text-[16px]`}
+                  >
+                    {trimURL}
+                  </a>
+                </Link>
+              );
+            })}
           </div>
         </div>
         <Button content="Contact us" type="button" />
--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -9,16 +9,8 @@ export const SEO = ({ title, metaContent }: Props) => {
   return (
     <Head>
       <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=5.0"
-      />
       <meta name="description" content={metaContent} />
       <meta property="og:type" content="website" />
-      <meta name="theme-color" content="#ffffff" />
-      <link type="image/svg" rel="icon" href="/favicon.svg" />
-      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
   );
 };
